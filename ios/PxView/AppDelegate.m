537f9c0707f0d0ccdf3ef938ec4b7afed75742e2
CyberDandy <mehdi.mlala@contentsquare.com>
2019-08-28 19:39:48 +0200

    Add Info.plist file to the project + change Podfile

--- ios/PxView/AppDelegate.m
+++ ios/PxView/AppDelegate.m
@@ -39,11 +39,9 @@
   return YES;
 }
 
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
-  sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(nonnull NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
 {
-  return [RCTLinkingManager application:application openURL:url
-                      sourceApplication:sourceApplication annotation:annotation];
+  return [RCTLinkingManager application:application openURL:url options:options];
 }
 
 @end
